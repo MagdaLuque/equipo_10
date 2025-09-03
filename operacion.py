a7bff236aa9ba5fc5d7a5adcfbe461b82b97895d
yugen611 <103396485+yugen611@users.noreply.github.com>
2025-09-03 11:58:54 -0400

    Add files via upload

---
+++ operacion.py
@@ -0,0 +1,9 @@
+# Operación simple
+numero1 = 5
+numero2 = 10
+suma= numero1 + numero2
+print("La suma de ", numero1, " y ",numero2," es ",suma)
+print("1 Luque Mamani Magdalena           CI 13087255")
+print("2 Pérez Ramirez Israel Fernando    CI 8359163")
+print("3 Sandoval Calderón Kevin Oscar    CI 9904973")
+print("4 Viscarra Chavez Leonel Pablo     CI 10908722")
