6b4245001353fb03e3aa11c5db2fb3c5596e97d5
Israel <israzorz@gmail.com>
2025-09-03 11:03:53 -0400

    trabajo de desarrollador 2 listo, archivo java

---
+++ saludo.java
@@ -0,0 +1,10 @@
+public class Saludo { 
+    public static void main(String[] args) { 
+        System.out.println("¡Hola, equipo numero 10!"); 
+        System.out.println("1  Perez Ramirez Israel Fernando"); 
+        System.out.println("2  Luque Mamani Magdalena"); 
+        System.out.println("3  Sandoval Calderon Kevin Oscar"); 
+        System.out.println("4  Viscarra Chavez Leonel Pablo"); 
+    
+    } 
+} 
